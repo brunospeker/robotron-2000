7615ae5ffe18de664a09f67e20291981b9c33109
Bruno Speker <bruno.speker@gmail.com>
2023-07-31 20:20:23 -0300

    Exercício da aula 5
    Adição de cores para os robos

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function manipulaDados(operacao) {
     }
 }
 */
-// Aula 4
+/* Aula 4
 const controle = document.querySelectorAll("[data-controle]")
 
 //Iteração dos elementos e refatoração da função
@@ -65,4 +65,74 @@ function manipulaDados(operacao, controle) {
     } else {
         peca.value = parseInt(peca.value) + 1
     }
+}*/
+
+// Aula 5
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+//Iteração dos elementos e refatoração da função
+controle.forEach( elemento => {
+	elemento.addEventListener("click", (evento) => {
+		manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+		atualizaEstatisticas(evento.target.dataset.peca, evento.target.dataset.controle)
+	})
+})
+
+function manipulaDados(operacao, controle) {
+	const peca = controle.querySelector("[data-contador]")
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca, operacao) {
+    if(operacao === "+") {
+        estatistica.forEach( (elemento) => {
+          elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+        })
+      } else {
+        estatistica.forEach( (elemento) => {
+          elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+        })
+      }
+}
+
+function trocaImagem(cor){
+   robo = document.querySelector(".robo").src="img/Robotron 2000 - " + cor + "/robotron.png";
 }
